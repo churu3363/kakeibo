96e03df4027fc904036ad53cdaeb5a319fc4bdb7
churu3363 <84961367+churu3363@users.noreply.github.com>
2026-09-02 23:27:28 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "kakeibo-v15-20260902";
+const CACHE = "kakeibo-v16-20260902";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", event => {
